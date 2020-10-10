23771389ce611e973569ad61c3e849d1291cfbc7
ZHENK <chengzhenyang@gmail.com>
2020-10-11 07:12:43 +1300

    fix stuff

--- T3/src/application/models/Product.java
+++ T3/src/application/models/Product.java
@@ -1,7 +1,5 @@
 package application.models;
 
-import java.util.Random;
-
 import application.SceneManager;
 import application.SceneManager.Scenes;
 import javafx.beans.property.SimpleStringProperty;
@@ -22,16 +20,15 @@ public class Product {
     }
 
     public void handleClick() {
-        Random rand = new Random();
-        switch (rand.nextInt(3))// default range is from(0 to 14) +1 at the end makes the range from(1 to 15)
+        switch (type.get())
         {
-            case 0:
+            case "Hotel Deal":
                 _sceneManager.switchScene(Scenes.HOTEL_DEAL);
                 break;
-            case 1:
+            case "Flight":
                 _sceneManager.switchScene(Scenes.FLIGHT);
                 break;
-            case 2:
+            case "Travel Deal":
                 _sceneManager.switchScene(Scenes.PACKAGE_DEAL);
                 break;
         }
